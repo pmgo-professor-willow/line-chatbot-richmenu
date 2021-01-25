02f4fb4883ebb954cd63260331391398f027ace1
Ze-Hao Wang <salmon.zh.tw@gmail.com>
2021-01-26 07:17:24 +0800

    update env key

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ dotenv.config();
 
 const client = new line.Client({
   channelAccessToken: process.env.TARGET === 'prod'
-    ? process.env.CHANNEL_ACCESS_TOKEN!
-    : process.env.CHANNEL_ACCESS_TOKEN_DEV!,
+    ? process.env.LINE_CHANNEL_ACCESS_TOKEN!
+    : process.env.LINE_CHANNEL_ACCESS_TOKEN_DEV!,
   channelSecret: process.env.TARGET === 'prod'
-    ? process.env.CHANNEL_SECRET!
-    : process.env.CHANNEL_SECRET_DEV!,
+    ? process.env.LINE_CHANNEL_SECRET!
+    : process.env.LINE_CHANNEL_SECRET_DEV!,
 });
 
 const deleteAllRichMenus = async () => {
